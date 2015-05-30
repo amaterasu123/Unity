fdc2f4942f68f7fd0fa3fef9302a8378e053fed7
student <student@MacBook-Pro-03.local>
2015-05-30 09:18:45 +0900

    ufo

---
+++ Assets/Playerscript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playerscript : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.position += this.transform.right * 0.05f;
+		}
+		else if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.position += this.transform.forward * 0.05f;
+		}
+		if(Input.GetKey(KeyCode.Space)){
+		transform.Translate (0, -0.05f, 0);
+		}
+	}
+}
